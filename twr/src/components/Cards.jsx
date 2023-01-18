b2e0a05c73ace12d656c23e04c2aef292b4df0a7
JMwira <jaymwira@gmail.com>
2023-01-18 01:54:54 -0800

    cardComponent

---
+++ twr/src/components/Cards.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import{
+    BsFillPersonCheckFill,
+    BsFillPersonDashFill,
+    BsFillPersonFill} 
+    from 'react-icons/bs'
+
+function Cards(){
+    return(
+        <div className='w-full py-[10rem] px-4 bg-white' >
+            <div className='max-w-[1100px] mx-auto grid md:grid-cols-3 gap-8' >
+                <div className='w-full p-5 shadow-xl rounded-md flex flex-col hover:scale-105 duration-300 ' >
+                    <BsFillPersonCheckFill className='text-lg mx-auto ' />
+                    <h2 className='text-2xl font-bold text-center py-3' >Single User</h2>
+                    <p className='text-center text-4xl font-bold' >$149</p>
+                    <div className='text-center font-medium' >
+                        <p className='py-2 border-b mx-8 mt-8'>500GB Storage</p>
+                        <p className='py-2 border-b mx-8'>1 Granted user</p>
+                        <p className='py-2 border-b mx-8'>Send up to 2GB</p>
+                    </div>
+                    <button className='bg-[#00df9a] rounded-md text-black font-medium my-6 mx-auto px-6 py-3' >Start trial</button>
+                </div>
+
+                
+                <div className='w-full p-5 shadow-xl bg-gray-100 rounded-md flex flex-col hover:scale-105 duration-300 ' >
+                    <BsFillPersonCheckFill className='text-lg mx-auto ' />
+                    <h2 className='text-2xl font-bold text-center py-3' >Single User</h2>
+                    <p className='text-center text-4xl font-bold' >$149</p>
+                    <div className='text-center font-medium' >
+                        <p className='py-2 border-b mx-8 mt-8'>500GB Storage</p>
+                        <p className='py-2 border-b mx-8'>1 Granted user</p>
+                        <p className='py-2 border-b mx-8'>Send up to 2GB</p>
+                    </div>
+                    <button className='text-[#00df9a] rounded-md bg-black font-medium my-6 mx-auto px-6 py-3' >Start trial</button>
+                </div>
+
+                
+                <div className='w-full p-5 shadow-xl rounded-md flex flex-col hover:scale-105 duration-300 ' >
+                    <BsFillPersonCheckFill className='text-lg mx-auto ' />
+                    <h2 className='text-2xl font-bold text-center py-3' >Single User</h2>
+                    <p className='text-center text-4xl font-bold' >$149</p>
+                    <div className='text-center font-medium' >
+                        <p className='py-2 border-b mx-8 mt-8'>500GB Storage</p>
+                        <p className='py-2 border-b mx-8'>1 Granted user</p>
+                        <p className='py-2 border-b mx-8'>Send up to 2GB</p>
+                    </div>
+                    <button className='bg-[#00df9a] rounded-md text-black font-medium my-6 mx-auto px-6 py-3' >Start trial</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- twr/src/components/NewsLetter.jsx
+++ twr/src/components/NewsLetter.jsx
@@ -4,7 +4,7 @@ function NewsLetter(){
     return(
         <div className='bg-black w-full grid py-20 h-auto text-white px-[60px] justify-center text-center ' >
             <h1 className='font-bold text-2xl' >Want tips and tricks to optimize your flow?</h1>
-            <Typed className='text-gray-500' strings={['Sign up to our newsletter and stay up o date','if yes']} typeSpeed={120} backSpeed={50} loop/>
+            <Typed className='text-gray-500' strings={['Sign up to our newsletter and stay up o date']} typeSpeed={120} backSpeed={50} loop/>
             <form className='' >
                 <input type="email" placeholder='your email here' className='p-2 my-2 rounded-md md:w-[75%] w-full' />
                 <button className='bg-[#00df9a] text-white py-2 rounded-md shadow-md px-3 mx-2' >Notify Me</button>
